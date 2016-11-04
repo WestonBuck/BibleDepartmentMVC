0843e85a3c3c4d7569984b831636f6734c7e8d66
keenan.gates <keenan.gates@C02L599YDV33W>
2016-11-04 13:24:10 -0500

    Indented table attributes for SQL Query

--- SQLDatabaseTablesCreate.sql
+++ SQLDatabaseTablesCreate.sql
@@ -3,33 +3,33 @@ Drop table GIFT;
 Drop table BADGE;
 
 CREATE TABLE USER_ (
-USER_ID int not null IDENTITY(1,1),
-FIRST_NAME varchar(100) not null,
-LAST_NAME varchar(100) not null,
-EMAIL varchar(100) not null,
-PHOTO_URL varchar(MAX),
-USER_TYPE varchar(10) not null,
-SHAREABLE_LINK varchar(MAX) not null
+	USER_ID int not null IDENTITY(1,1),
+	FIRST_NAME varchar(100) not null,
+	LAST_NAME varchar(100) not null,
+	EMAIL varchar(100) not null,
+	PHOTO_URL varchar(MAX),
+	USER_TYPE varchar(10) not null,
+	SHAREABLE_LINK varchar(MAX) not null
 );
 
 CREATE TABLE GIFT (
-GIFT_ID int not null IDENTITY(1,1),
-BADGE_ID int not null,
-SENDER_ID int not null,
-RECIPIENT_ID int not null,
-TREE_LOC_X int not null,
-TREE_LOC_Y int not null,
-COMMENT varchar(140)
+	GIFT_ID int not null IDENTITY(1,1),
+	BADGE_ID int not null,
+	SENDER_ID int not null,
+	RECIPIENT_ID int not null,
+	TREE_LOC_X int not null,
+	TREE_LOC_Y int not null,
+	COMMENT varchar(140)
 );
 
 CREATE TABLE BADGE (
-BADGE_ID int not null,
-TYPE varchar(MAX),
-RETIREMENT_DATE date,
-START_DATE date not null,
-NAME varchar(MAX),
-SELF_GIVE bit,
-STUDENT_GIVE bit,
-STAFF_GIVE bit,
-FACULTY_GIVE bit
+	BADGE_ID int not null,
+	TYPE varchar(MAX),
+	RETIREMENT_DATE date,
+	START_DATE date not null,
+	NAME varchar(MAX),
+	SELF_GIVE bit,
+	STUDENT_GIVE bit,
+	STAFF_GIVE bit,
+	FACULTY_GIVE bit
 );
