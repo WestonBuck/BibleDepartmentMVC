bdc9670aab3723317c826bed76216232b2d2d853
DemonWav <demonwav@gmail.com>
2016-11-05 14:15:14 -0500

    General file and usings cleanup

--- MVC Badge System/MVC Badge System/Models/Badge.cs
+++ MVC Badge System/MVC Badge System/Models/Badge.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace MVC_Badge_System.Models
 {
--- MVC Badge System/MVC Badge System/Models/User.cs
+++ MVC Badge System/MVC Badge System/Models/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MVC_Badge_System.Models
+﻿namespace MVC_Badge_System.Models
 {
     public class User
     {
