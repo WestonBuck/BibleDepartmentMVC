04053f271c9e486b9d3c40f2759afac3a017093d
valcus <pseudojap@gmail.com>
2016-11-05 16:24:14 -0500

    added connection string

--- MVC Badge System/MVC Badge System/Db/Db.cs
+++ MVC Badge System/MVC Badge System/Db/Db.cs
@@ -2,5 +2,6 @@
 {
     public class Db
     {
+        public const string CONNECTION = "Data Source=.\\SQLEXPRESS;Initial Catalog=GSTdata;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
 }
