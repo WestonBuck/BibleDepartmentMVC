8be58eb5418c5e660ce3afeecbf0b5f77f9149b8
DemonWav <demonwav@gmail.com>
2016-11-07 15:36:19 -0600

    Fix buld and run

---
+++ MVC Badge System/MVC Badge System/Controllers/EmailTestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MVC_Badge_System.Controllers
+{
+    public class EmailTestController : Controller
+    {
+        // GET: EmailTest
+        public ActionResult Index()
+        {
+            // This actually emails me, this is for demo purposes only. Please don't actually use this.
+            EmailManager.SendTextEmail("This is a test email.", "Test Email", "DemonWav <demonwav@gmail.com>", "Test User", "noreply",
+                response =>
+                {
+                    Console.WriteLine("Email successfully sent.");
+                });
+
+            return View();
+        }
+    }
+}
