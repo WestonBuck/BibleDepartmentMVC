0b8c410d4dc25c41013e7996b7c780a9accaf63e
valcus <pseudojap@gmail.com>
2016-11-15 21:42:26 -0600

    Added Prerequisite Model

---
+++ MVC Badge System/MVC Badge System/Models/Prerequisite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Badge_System.Models
+{
+    public class Prerequisite
+    {
+        public int PrerequisiteId { get; set; }
+        public int ParentId { get; set; }
+        public int ChildId { get; set; }
+
+        public Badge Parent { get; set; }
+        public Badge Child { get; set; }
+    }
+}
