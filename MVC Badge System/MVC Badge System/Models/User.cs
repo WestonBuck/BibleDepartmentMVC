c112420c60fa44896a3cdf62e5fd4944e3bf9848
Emily <emily.pielemeier@eagles.oc.edu>
2016-11-27 17:10:42 -0600

    Update display and add validations for users

--- MVC Badge System/MVC Badge System/Models/User.cs
+++ MVC Badge System/MVC Badge System/Models/User.cs
@@ -5,19 +5,25 @@ namespace MVC_Badge_System.Models
 {
     public class User
     {
+        [DisplayName("ID")]
         public int UserId { get; set; }
         [DisplayName("First Name")]
+        [Required(ErrorMessage = "First Name required")]
         public string FirstName { get; set; }
         [DisplayName("Last Name")]
+        [Required(ErrorMessage = "Last Name required")]
         public string LastName { get; set; }
         [DisplayName("Email")]
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email required")]
         public string Email { get; set; }
         [DisplayName("Photo URL")]
         [DataType(DataType.ImageUrl)]
         public string PhotoUrl { get; set; }
-        [DisplayName("User Type")]
+        [DisplayName("Type")]
+        [Required(ErrorMessage = "User Type required")]
         public string UserType { get; set; }
+        [DisplayName("Shareable Link")]
         [DataType(DataType.Url)]
         public string ShareableLink { get; set; }
 
