4647490fcfa94e40aff1ecb9cfb5fd37c6975180
Emily <emily.pielemeier@eagles.oc.edu>
2016-11-27 12:37:11 -0600

    removed ID dependency on search bars so multiple can be used on the same page

--- MVC Badge System/MVC Badge System/Content/SearchModule.js
+++ MVC Badge System/MVC Badge System/Content/SearchModule.js
@@ -4,15 +4,15 @@
 
 window.SearchModule = (function ($) {
     // Holds any functions needed to handle searches
-    var selectHandler; // line 41
-    var getUser = function (userName) {
+    var selectHandlers = []; // line 41
+    var getUser = function (userName, callback) {
         $.ajax({
             url: '/User/GetUser',
             type: 'POST',
             dataType: 'html',
             data: { filter: userName },
             success: function (data) {
-                $('#results').html(data);
+                callback(data);
            },
             error: function () { }
         });
@@ -21,9 +21,9 @@ window.SearchModule = (function ($) {
     // returns the user ID for the selection in the search bar based on
     //the options in the drop down datalist
     ////////////////////////////////////////////////////////////////////
-    var getSelectedUserID = function () {
-        var val = $("#txtSearch").val();
-        var userID = $('#user_list option').filter(function () {
+    var getSelectedUserID = function (parentDiv) {
+        var val = parentDiv.chidren('input[type=search]').val();
+        var userID = parentDiv.find('option').filter(function () {
             return this.value === val;
         }).first().attr("name");
 
@@ -38,46 +38,54 @@ window.SearchModule = (function ($) {
     //////////////////////////////////////////////////////////////
     // Sets our placeholder function to the user's search function
     //////////////////////////////////////////////////////////////
-    var setSelectHandler = function (handlerFunc) {
-        selectHandler = handlerFunc;
+    var setSelectHandler = function (parentDivID, handlerFunc) {
+        selectHandlers[parentDivID] = handlerFunc;
     };
     return {
         init: function () {
             // when a user modifies the text in the search bar, it modifies
             //the datalist by calling the get user function
-            $('#txtSearch').keyup(function (e) {
+            $('.gst-studentsearch input[type=search]').keyup(function (e) {
                 if ((e.which >= 48 && e.which <= 90) || e.which === 8 || e.which === 109 || e.which === 110 || e.which === 189 || e.which === 190) {
-                    var userName = $('#txtSearch').val();
-                    getUser(userName);
-                    // when an option is selected by mouseclick, it calls a different function
-                    //depending on the requirements of the page
-                    $('#user_list option').click(function (e) {
-                        if (typeof selectHandler === 'function') {
-                            selectHandler();
+                    var thisBar = $(this);
+                    var userName = thisBar.val();
+                    
+                    //AJAX get the users that match, and on success complete the function:
+                    getUser(userName, function (postResults) {
+                
+                        var resultsList = thisBar.parent().children('.gst-ss-results');
+                        if (resultsList) {
+                            resultsList.html(postResults);
                         }
                     });
                 }
             });
 
-            // when an option is selected via enter, it calls a different function
+            // when an option is selected by mouseclick, it calls a different function
             //depending on the requirements of the page
-            $('#txtSearch').keypress(function (e) {
-                if (e.which === 13 && typeof selectHandler === 'function') {
-                    selectHandler();
+            $('.gst-studentsearch input[type=search]').on('input', function (e) {
+                var selected = $(this).val();
+                var parentDiv = $(this).parent();
+                var options = parentDiv.find('option');
+                for (var i = 0; i < options.length; i++) {
+                    //user selected a datalist option
+                    if (options[i].value === selected) {
+                        console.log('selected on click');
+                        var parentDivID = parentDiv.attr('id');
+                        var onSelectFunc = selectHandlers[parentDivID];
+                        if (typeof onSelectFunc === 'function') {
+                            onSelectFunc(selected);
+                        }
+                    }
                 }
             });
         },
-        // line 8
-        getUser: function (userName) {
-            getUser(userName);
-        },
         // line 24
-        getSelectedUserID: function (){
-            getSelectedUserID();
+        getSelectedUserID: function (studentSearchDiv) {
+            getSelectedUserID(studentSearchDiv);
         },
-        // line 41
-        setSelectHandler: function(handlerFunc){
-            setSelectHandler(handlerFunc);
+        setSelectHandler: function (studentSearchDivID, handlerFunc) {
+            setSelectHandler(studentSearchDivID, handlerFunc);
         }
     };
 })(jQuery);
