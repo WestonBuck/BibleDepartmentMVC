e938f88c13a8a94ee773fb746a1e915111c4fb1a
Emily <emily.pielemeier@eagles.oc.edu>
2016-12-07 10:58:43 -0600

    add basic scaffolding for CRUD

--- MVC Badge System/MVC Badge System/Controllers/BadgeController.cs
+++ MVC Badge System/MVC Badge System/Controllers/BadgeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Badge_System.Models;
 
 namespace MVC_Badge_System.Controllers
 {
@@ -12,6 +13,48 @@ namespace MVC_Badge_System.Controllers
         public ActionResult List()
         {
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            Badge badge = new Badge();
+            return View(badge);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Badge badge)
+        {
+            Db.Db.CreateBadge(badge);
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Badge badge = Db.Db.GetBadge(id);
+            return View(badge);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Badge badge)
+        {
+            Db.Db.UpdateBadge(badge);
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Badge badge = Db.Db.GetBadge(id);
+            return View(badge);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Badge badge = Db.Db.GetBadge(id);
+            if (badge != null)
+            {
+                Db.Db.DeleteBadge(badge);
+            }
+            return RedirectToAction("List");
         }
     }
 }
